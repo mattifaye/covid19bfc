efc8e7673146ddc539d60f4e5278dad4beb27868
Matti Faye <matti.faye@gmail.com>
2020-04-08 14:31:10 +0200

    Add files via upload

--- telechargement.py
+++ telechargement.py
@@ -1,34 +1,34 @@
 import urllib3 as urllib
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1095809811&single=true&output=csv'
-urllib.urlretrieve(url, 'retourdom.csv')
+urllib.request.urlretrieve(url, 'retourdom.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1142885751&single=true&output=csv'
-urllib.urlretrieve(url, 'carte.csv')
+urllib.request.urlretrieve(url, 'carte.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=125960350&single=true&output=csv'
-urllib.urlretrieve(url, 'bfc.csv')
+urllib.request.urlretrieve(url, 'bfc.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1779906158&single=true&output=csv'
-urllib.urlretrieve(url, '21.csv')
+urllib.request.urlretrieve(url, '21.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1018863531&single=true&output=csv'
-urllib.urlretrieve(url, '25.csv')
+urllib.request.urlretrieve(url, '25.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1346077964&single=true&output=csv'
-urllib.urlretrieve(url, '39.csv')
+urllib.request.urlretrieve(url, '39.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1100340057&single=true&output=csv'
-urllib.urlretrieve(url, '58.csv')
+urllib.request.urlretrieve(url, '58.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=2032450000&single=true&output=csv'
-urllib.urlretrieve(url, '70.csv')
+urllib.request.urlretrieve(url, '70.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=224371307&single=true&output=csv'
-urllib.urlretrieve(url, '71.csv')
+urllib.request.urlretrieve(url, '71.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1653344045&single=true&output=csv'
-urllib.urlretrieve(url, '89.csv')
+urllib.request.urlretrieve(url, '89.csv')
 
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=711918987&single=true&output=csv'
-urllib.urlretrieve(url, '90.csv')
+urllib.request.urlretrieve(url, '90.csv')
