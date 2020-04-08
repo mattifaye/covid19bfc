6243d5f5ebc449bd7f005c7593dfbf87f8164b2e
Matti Faye <matti.faye@gmail.com>
2020-04-08 15:04:05 +0200

    Add files via upload

--- telechargement.py
+++ telechargement.py
@@ -1,7 +1,56 @@
 import requests
 
-
 url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1095809811&single=true&output=csv'
 r = requests.get(url, allow_redirects=True)
 
 open('retourdom.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1142885751&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('carte.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=125960350&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('bfc.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1779906158&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('21.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1018863531&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('25.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1346077964&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('39.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1100340057&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('58.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=2032450000&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('70.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=224371307&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('71.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1653344045&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('89.csv', 'wb').write(r.content)
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=711918987&single=true&output=csv'
+r = requests.get(url, allow_redirects=True)
+
+open('90.csv', 'wb').write(r.content)
