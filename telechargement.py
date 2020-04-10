8f27e89c97bb13121e743e9af75a4d9c4333d8c8
mattifaye <matti.faye@gmai.com>
2020-04-10 15:13:03 +0200

    Cleaner and more reliable way to process data

--- telechargement.py
+++ telechargement.py
@@ -1,19 +1,39 @@
-import requests
-import time
+import pandas as pd
 
-urlsEtfichiers = {'https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1095809811&single=true&output=csv': 'retourdom.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1142885751&single=true&output=csv': 'carte.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=125960350&single=true&output=csv': 'bfc.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1779906158&single=true&output=csv': '21.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1018863531&single=true&output=csv': '25.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1346077964&single=true&output=csv': '39.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1100340057&single=true&output=csv': '58.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=2032450000&single=true&output=csv': '70.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=224371307&single=true&output=csv': '71.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=1653344045&single=true&output=csv': '89.csv','https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=711918987&single=true&output=csv': "90.csv","https://docs.google.com/spreadsheets/d/e/2PACX-1vTs_XIZk2vydeg_0cw6hn9_YTNAJ9679Uw03aa7PogpQqL6G7a838mrNjZInh_pgthpYBLEa9R7iJcF/pub?gid=686373021&single=true&output=csv" : "max.csv"}
+url = "https://www.data.gouv.fr/fr/datasets/r/63352e38-d353-4b54-bfd1-f1b3ee1cabd7"
+dptBFC = ["21", "25", "39", "58", "70", "71", "89", "90"]
 
-for url, fichier in urlsEtfichiers.items(): 
-    r = requests.get(url, allow_redirects=True)
-    open(fichier, 'wb').write(r.content)
-    
-    with open(fichier, 'r') as file:
-        contents = file.read()
-        while 'Lignes' in contents:
-            print ('On redownload')
-            time.sleep(10)
-            r = requests.get(url, allow_redirects=True)
-            open(fichier, 'wb').write(r.content)
-            contents = open(fichier, 'r').read()
-        else:
-            print ('Tout est OK')
+# On importe les données depuis l'url
+source = pd.read_csv(url,sep=";")
+
+# On filtre ce qui nous intéresse
+filtre = source[(source["sexe"] == 0) & (source["dep"].isin(dptBFC))].filter(["jour","dep","dc","rea","hosp","rad"])
+
+# On renomme les colonnes pour que ce soit plus lisible
+donneesOK = filtre.rename(index=str, columns={"jour":"Date","dep":"Code","hosp":"Personnes hospitalisées","rea":"Personnes en réanimation","rad":"Personnes de retour à domicile (cumul)","dc":"Personnes décédées (cumul)"})
+
+# On créé le fichier bfc et on l'enregistre
+bfc = pd.pivot_table(donneesOK, values=["Personnes décédées (cumul)","Personnes en réanimation","Personnes hospitalisées"], index=["Date"], aggfunc="sum")
+bfc.to_csv("bfc.csv")
+
+# On créé le fichier retourdom et on l'enregistre
+retourdom = pd.pivot_table(donneesOK, values=["Personnes de retour à domicile (cumul)","Personnes décédées (cumul)"], index=["Date"], aggfunc="sum")
+retourdom.to_csv("retourdom.csv")
+
+# On trouve la valeur maximale dans tous les départements et on l'enregistre
+maximum = ((source[(source["sexe"] == 0) & (source["dep"].isin(dptBFC))].filter(["dc","rea","hosp"]).max()).max()+10)
+f = open("max.csv", "w")
+f.write(str(maximum))
+f.close()
+
+# On récupère les décès pour la carte et on les enregistre
+sourcecarte = pd.read_csv("sourcecarte.csv",sep=",",dtype={"Code": object})
+decespardpt = pd.pivot_table(donneesOK, values=["Personnes décédées (cumul)"], index=["Code"], aggfunc="last")
+carte = pd.merge(sourcecarte, decespardpt, left_on="Code", right_index= True, how="outer")
+carte.to_csv("carte.csv", index= False)
+
+# On créé un fichier pour chaque département et on les enregistre
+for dpt in dptBFC:
+    filtre = source[(source["sexe"] == 0) & (source["dep"] == dpt)].filter(["jour","dep","dep","dc","rea","hosp"])
+    donneesOK = filtre.rename(index=str, columns={"jour":"Date","dep":"Code","hosp":"Personnes hospitalisées","rea":"Personnes en réanimation","rad":"Personnes de retour à domicile (cumul)","dc":"Personnes décédées (cumul)"})
+    donneesOK.to_csv(dpt+".csv", index = False)
