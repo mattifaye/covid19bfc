636189ba428a74683a83aebdc7e29233366cf3fd
Matti Faye <matti.faye@gmai.com>
2020-10-15 08:20:46 +0200

    Update telechargement.py

--- taux-incidence/telechargement.py
+++ taux-incidence/telechargement.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 #Import fichiers
-df = pd.read_csv("https://www.data.gouv.fr/fr/datasets/r/406c6a23-e283-4300-9484-54e78c8ae675", sep=";", dtype={"dep":object})
-df2 = pd.read_csv("popBFC.csv", dtype={"dep":object})
+df = pd.read_csv("https://www.data.gouv.fr/fr/datasets/r/19a91d64-3cd3-42fc-9943-d635491a4d76", sep=";", dtype={"dep":object})
 
 # Filtres
 depBFC = ["21","25","39","58","70","71","89","90"]
@@ -10,17 +9,17 @@ age = ["0"]
 
 #Fusion des deux sources de données
 df = df[df["dep"].isin(depBFC) & df["cl_age90"].isin(age)]
-df3 = pd.merge(df, df2, on="dep")
+
 
 # Calcul taux d'incidence quotidien
-df3["taux_incidence_quot"] = df3["P"]/df3["pop"]*100000
+df["taux_incidence_quot"] = df["P"]/df["pop"]*100000
 
 # Calcul taux d'incidence par département sur sept jours glissants
 for dep in depBFC:
-    df4 = df3[df3["dep"] == dep]
-    df4["positif_7j"] = df4["P"].rolling(7).sum()
-    df4["taux_incid_7j"] = df4["positif_7j"]/df4["pop"]*100000
-    df4.to_csv(dep+"_incidence7j.csv", index = False)
+    df2 = df[df["dep"] == dep]
+    df2["positif_7j"] = df2["P"].rolling(7).sum()
+    df2["taux_incid_7j"] = df2["positif_7j"]/df2["pop"]*100000
+    df2.to_csv(dep+"_incidence7j.csv",index=False)
 
 df_21 = pd.read_csv("21_incidence7j.csv")
 df_25 = pd.read_csv("25_incidence7j.csv")
@@ -31,7 +30,6 @@ df_71 = pd.read_csv("71_incidence7j.csv")
 df_89 = pd.read_csv("89_incidence7j.csv")
 df_90 = pd.read_csv("90_incidence7j.csv")
 
-# Regroupement de toutes les données dans un seul fichier
 df_bfc = pd.concat([df_21,df_25,df_39,df_58,df_70,df_71,df_89,df_90])
 df_bfc["dep_ok"] = df_bfc["dep"].replace({21:"Côte-d'Or",25:"Doubs",39:"Jura",58:"Nièvre",70:"Haute-Saône",71:"Saône-et-Loire",89:"Yonne",90:"Territoire de Belfort"})
 df_bfc.to_csv("bfc_incidence7j.csv",index=False)
