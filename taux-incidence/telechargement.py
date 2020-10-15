d1493aa62eec6d997d9958463fa32af570dc6d14
Matti Faye <matti.faye@gmai.com>
2020-10-15 08:18:14 +0200

    Simplification

--- taux-incidence/telechargement.py
+++ taux-incidence/telechargement.py
@@ -15,9 +15,9 @@ df3 = pd.merge(df, df2, on="dep")
 # Calcul taux d'incidence quotidien
 df3["taux_incidence_quot"] = df3["P"]/df3["pop"]*100000
 
+# Calcul taux d'incidence par département sur sept jours glissants
 for dep in depBFC:
     df4 = df3[df3["dep"] == dep]
-#    df4["P_decale"] = df4["P"].shift(periods=-3)
     df4["positif_7j"] = df4["P"].rolling(7).sum()
     df4["taux_incid_7j"] = df4["positif_7j"]/df4["pop"]*100000
     df4.to_csv(dep+"_incidence7j.csv", index = False)
@@ -31,6 +31,7 @@ df_71 = pd.read_csv("71_incidence7j.csv")
 df_89 = pd.read_csv("89_incidence7j.csv")
 df_90 = pd.read_csv("90_incidence7j.csv")
 
+# Regroupement de toutes les données dans un seul fichier
 df_bfc = pd.concat([df_21,df_25,df_39,df_58,df_70,df_71,df_89,df_90])
 df_bfc["dep_ok"] = df_bfc["dep"].replace({21:"Côte-d'Or",25:"Doubs",39:"Jura",58:"Nièvre",70:"Haute-Saône",71:"Saône-et-Loire",89:"Yonne",90:"Territoire de Belfort"})
 df_bfc.to_csv("bfc_incidence7j.csv",index=False)
