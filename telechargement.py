59e025f260bd3478c1430e8e8b3937a0fa17b19f
Matti Faye <matti.faye@gmail.com>
2020-10-21 11:42:04 +0200

    Update telechargement.py

--- telechargement.py
+++ telechargement.py
@@ -36,7 +36,29 @@ df_90 = pd.read_csv("90_incidence7j.csv")
 
 df_dep = pd.concat([df_21,df_25,df_39,df_58,df_70,df_71,df_89,df_90])
 df_dep["dep_ok"] = df_dep["dep"].replace({21:"Cote-d'Or",25:"Doubs",39:"Jura",58:"Nievre",70:"Haute-Saone",71:"Saone-et-Loire",89:"Yonne",90:"Territoire de Belfort"})
-df_dep.to_csv("dep_incidence7j.csv",index=False,quoting=1)
+df_dep.to_csv("dep_incidence7j.csv",index=False)
+
+
+####### Taux incidence avec accents pour test
+for dep in depBFC:
+    df2 = df[df["dep"] == dep]
+    df2["positif_7j"] = df2["P"].rolling(7).sum()
+    df2["taux_incid_7j"] = df2["positif_7j"]/df2["pop"]*100000
+    df2.to_csv(dep+"_incidence7j.csv",index=False)
+
+df_21 = pd.read_csv("21_incidence7j.csv")
+df_25 = pd.read_csv("25_incidence7j.csv")
+df_39 = pd.read_csv("39_incidence7j.csv")
+df_58 = pd.read_csv("58_incidence7j.csv")
+df_70 = pd.read_csv("70_incidence7j.csv")
+df_71 = pd.read_csv("71_incidence7j.csv")
+df_89 = pd.read_csv("89_incidence7j.csv")
+df_90 = pd.read_csv("90_incidence7j.csv")
+
+df_dep = pd.concat([df_21,df_25,df_39,df_58,df_70,df_71,df_89,df_90])
+df_dep["dep_ok"] = df_dep["dep"].replace({21:"Côte-d'Or",25:"Doubs",39:"Jura",58:"Nièvre",70:"Haute-Saône",71:"Saône-et-Loire",89:"Yonne",90:"Territoire de Belfort"})
+df_dep.to_csv("dep_incidence7j_accents.csv",index=False)
+
 
 # Calcul taux d'incidence régional sur sept jours glissants
 tcd = df.pivot_table(index=["jour"],values=["P","pop"],aggfunc=sum)
